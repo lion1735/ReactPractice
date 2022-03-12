5c6ab453e607c94e780b3ebde3ccc8348c78ece0
lion1735 <lion1735@naver.com>
2022-03-13 01:43:36 +0900

    5.1 css module

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Wlecom back!</h1>
+      <h1 className={styles.title}>Wlecom back!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
