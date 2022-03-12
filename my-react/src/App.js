2bf6f51cfbe548105a3e6c551cb3c1012d9b8576
lion1735 <lion1735@naver.com>
2022-03-13 02:50:23 +0900

    6.2 Deps - 특정 데이터가 바뀔때마다 실행하기

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,22 +1,40 @@
 import { useEffect, useState } from "react";
 
-// (10)onClick -> (6)onClick -> setCnt -> prev+1 -> cnt -> (9) <h1> {cnt} </h1>
 function App() {
   const [cnt, setCnt] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCnt((prev) => prev + 1);
-  console.log("i run all the time"); //console.log 한 번만 실행하기?
-  //1. 한 번 실행하기
-  // const iRunOnlyOnce = () => {
-  //   console.log("i run only once.");
-  // };
-  // useEffect(iRunOnlyOnce, []); //user Effect : 한 번만 실행하기
-  //2. 한 번 실행하기 - 간소화
+
+  //input 값이 바뀌었을때 실행될 함수
+  const onChange = (event) => setKeyword(event.target.value);
+
+  //항상 실행하기
+  // console.log("i run all the time");
+
+  //한 번만 실행하기 -> [] 안에 변화되는 데이터가 없기때문에 바뀔일이 없다.
   useEffect(() => {
     console.log("CALL THE API");
   }, []);
 
+  //keyword가 변화할때만 실행하기 [] <- 변화를 감지할 변수를 넣기
+  useEffect(() => {
+    if (keyword !== "" && cnt > 5) {
+      console.log("I run when 'keyword' changes", keyword);
+    }
+  }, [keyword]);
+  //cnt 변화할때만 실행하기
+  useEffect(() => {
+    console.log("I run when 'cnt' changes", cnt);
+  }, [cnt]);
+
+  //cnt 혹은 keyword 가 변화할때 실행하기
+  useEffect(() => {
+    console.log("I run when 'cnt&keyword' changes", cnt, keyword);
+  }, [cnt, keyword]);
+
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{cnt}</h1>
       <button onClick={onClick}>click</button>
     </div>
