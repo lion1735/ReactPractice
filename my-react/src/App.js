c0052b6337d76584c5c391ddd8303b6ba51a1b8b
lion1735 <lion1735@naver.com>
2022-03-13 02:15:05 +0900

    6.0 counter

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
+// (10)onClick -> (6)onClick -> setCnt -> prev+1 -> cnt -> (9) <h1> {cnt} </h1>
 function App() {
+  const [cnt, setCnt] = useState(0);
+  const onClick = () => setCnt((prev) => prev + 1);
+  console.log("render"); //console.log 한 번만 실행하기?
   return (
     <div>
-      <h1 className={styles.title}>Wlecom back!</h1>
-      <Button text={"Continue"} />
+      <h1>{cnt}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
